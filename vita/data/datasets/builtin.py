38f026b8467c7978d1db38616427b54ac3b72b70
jaehyunkang-research <abyssinial@naver.com>
2023-11-29 06:35:20 +0000

    update builtin to use debug, single val

--- vita/data/datasets/builtin.py
+++ vita/data/datasets/builtin.py
@@ -16,10 +16,14 @@ from .mevis import register_mevis_instances
 _PREDEFINED_SPLITS_mevis = {
     "mevis_train": ("mevis/train",
                    "mevis/train/meta_expressions.json"),
-    "mevis_val": ("mevis/valid_u",
-                 "mevis/valid_u/meta_expressions.json"),
-    "mevis_test": ("mevis/valid",
-                  "mevis/valid/meta_expressions.json"),
+    "mevis_debug": ("mevis/dummy",
+                     "mevis/dummy/meta_expressions.json"),
+    "mevis_val": ("mevis/valid",
+                 "mevis/valid/meta_expressions.json"),
+    "mevis_test": ("mevis/test",
+                  "mevis/test/meta_expressions.json"),
+    "mevis_val_single": ("mevis/valid_single",
+                 "mevis/valid_single/meta_expressions.json"),
 }
 
 
