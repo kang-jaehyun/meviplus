481815713430f7c5234f84f5662bede2b3fda9f0
jaehyunkang-research <abyssinial@naver.com>
2024-01-03 14:58:28 +0000

    fix bug

--- genvis/genvis_model.py
+++ genvis/genvis_model.py
@@ -285,8 +285,8 @@ class Genvis(Vita):
         cur_fpn = self.lateral_conv(x)
         
         for i in range(self.feature_utilize_level):
-            y = cur_fpn + F.interpolate(self.multiscale_feature_proj[i](enhanced_features[i].permute(0,3,1,2)), size=cur_fpn.shape[-2:], mode="bilinear", align_corners=False)
-        y = self.output_conv(y)
+            cur_fpn = cur_fpn + F.interpolate(self.multiscale_feature_proj[i](enhanced_features[i].permute(0,3,1,2)), size=cur_fpn.shape[-2:], mode="bilinear", align_corners=False)
+        y = self.output_conv(cur_fpn)
         
         return y, enhanced_features
         
