805ab27f22c89b2d6be7836dcc062e68c3c1e395
jaehyunkang-research <abyssinial@naver.com>
2024-01-17 07:29:57 +0000

    cosine bce to supcon

--- genvis/genvis_model.py
+++ genvis/genvis_model.py
@@ -69,7 +69,7 @@ class Genvis(Vita):
 
         self.convlstm = ConvLSTMCell(input_dim=96 * roi_feature_level, hidden_dim=mask2former_hidden_dim, kernel_size=(3,3), bias=True) # TODO hidden dim configurable
         self.roi_feature_shape = (14,14)
-        
+        self.grounding_criterion = SupConLoss()
         self.xdecoder = xdecoder.eval().cuda()
         
         # freeze xdecoder
@@ -287,13 +287,22 @@ class Genvis(Vita):
         motion_emb = self.motion2text(last_hidden_state) # cQ, B, C
         # motion_emb = self.motion2text(h.reshape(cQ, B, h.shape[-3], feature_shape[0]*feature_shape[1]))
 
-        sim = F.cosine_similarity(motion_emb, class_embeddings, dim=-1).permute(1,0) # B, cQ
-        labels = torch.zeros_like(sim)
+        # sim = F.cosine_similarity(motion_emb, class_embeddings, dim=-1).permute(1,0) # B, cQ
+        # labels = torch.zeros_like(sim)
+        labels = torch.zeros(B, cQ).to(self.device)
         for i, ind_set in enumerate(positive_indices):
             labels[i][list(ind_set)] = 1
         
-        pos_weight =  ((B*cQ - labels.sum()) / labels.sum()).clamp(max=5)
-        grounding_loss_dict = {"loss_grounding" : F.binary_cross_entropy_with_logits(sim, labels, pos_weight=pos_weight)}
+        
+        loss_batchsum = torch.zeros(1).to(self.device)
+        motions = torch.unbind(motion_emb, dim=1)
+        # sentences = torch.unbind(class_embeddings, dim=0)
+        for m,s,l in zip(motions, class_embeddings, labels):
+            features = torch.stack([m, s[None].repeat_interleave(cQ, dim=0)], dim=1)
+            loss_batchsum += self.grounding_criterion(features, l)
+        
+        # pos_weight =  ((B*cQ - labels.sum()) / labels.sum()).clamp(max=5)
+        grounding_loss_dict = {"loss_grounding" : loss_batchsum/B}
         grounding_loss_dict_keys = list(grounding_loss_dict.keys())
         
         for k in grounding_loss_dict_keys:
@@ -655,3 +664,91 @@ class ConvLSTMCell(nn.Module):
         height, width = image_size
         return (torch.zeros(batch_size, self.hidden_dim, height, width, device=device),
                 torch.zeros(batch_size, self.hidden_dim, height, width, device=device))
+        
+class SupConLoss(nn.Module):
+    """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
+    It also supports the unsupervised contrastive loss in SimCLR"""
+    def __init__(self, temperature=0.07, contrast_mode='all',
+                 base_temperature=0.07):
+        super(SupConLoss, self).__init__()
+        self.temperature = temperature
+        self.contrast_mode = contrast_mode
+        self.base_temperature = base_temperature
+
+    def forward(self, features, labels=None, mask=None):
+        """Compute loss for model. If both `labels` and `mask` are None,
+        it degenerates to SimCLR unsupervised loss:
+        https://arxiv.org/pdf/2002.05709.pdf
+
+        Args:
+            features: hidden vector of shape [bsz, 2, feature_dim].
+            labels: ground truth of shape [bsz].
+            mask: contrastive mask of shape [bsz, bsz], mask_{i,j}=1 if sample j
+                has the same class as sample i. Can be asymmetric.
+        Returns:
+            A loss scalar.
+        """
+        device = (torch.device('cuda')
+                  if features.is_cuda
+                  else torch.device('cpu'))
+
+        if len(features.shape) < 3:
+            raise ValueError('`features` needs to be [bsz, n_views, ...],'
+                             'at least 3 dimensions are required')
+        if len(features.shape) > 3:
+            features = features.view(features.shape[0], features.shape[1], -1)
+
+        batch_size = features.shape[0]
+        if labels is not None and mask is not None:
+            raise ValueError('Cannot define both `labels` and `mask`')
+        elif labels is None and mask is None:
+            mask = torch.eye(batch_size, dtype=torch.float32).to(device)
+        elif labels is not None:
+            labels = labels.contiguous().view(-1, 1)
+            if labels.shape[0] != batch_size:
+                raise ValueError('Num of labels does not match num of features')
+            mask = torch.eq(labels, labels.T).float().to(device)
+        else:
+            mask = mask.float().to(device)
+
+        contrast_count = features.shape[1]
+        contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)
+        if self.contrast_mode == 'one':
+            anchor_feature = features[:, 0]
+            anchor_count = 1
+        elif self.contrast_mode == 'all':
+            anchor_feature = contrast_feature
+            anchor_count = contrast_count
+        else:
+            raise ValueError('Unknown mode: {}'.format(self.contrast_mode))
+
+        # compute logits
+        anchor_dot_contrast = torch.div(
+            torch.matmul(anchor_feature, contrast_feature.T),
+            self.temperature)
+        # for numerical stability
+        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
+        logits = anchor_dot_contrast - logits_max.detach()
+        # tile mask
+        mask = mask.repeat(anchor_count, contrast_count)
+        # mask-out self-contrast cases
+        logits_mask = torch.scatter(
+            torch.ones_like(mask),
+            1,
+            torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
+            0
+        )
+        mask = mask * logits_mask
+
+        # compute log_prob
+        exp_logits = torch.exp(logits) * logits_mask
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+        # compute mean of log-likelihood over positive
+        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
+
+        # loss
+        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
+        loss = loss.view(anchor_count, batch_size).mean()
+
+        return loss
